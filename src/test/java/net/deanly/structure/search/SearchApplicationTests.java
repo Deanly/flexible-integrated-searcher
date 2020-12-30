0d26215daf7e6426a273888ed50dd167f0fcbf69
deanly <deanly88@gmail.com>
2020-12-31 01:34:44 +0900

    init

---
+++ src/test/java/net/deanly/structure/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package net.deanly.structure.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
